6361dca7ba03190f7cd813ee6bb949c4440b21fe
Brandon Dewitt <brandonsdewitt@gmail.com>
2019-01-17 08:48:34 -0700

    attempt a decoder

--- ext/ruby_protobuf/Varinter.java
+++ ext/ruby_protobuf/Varinter.java
@@ -21,6 +21,7 @@ import org.jruby.RubyInteger;
 import org.jruby.RubyNumeric;
 import org.jruby.RubyString;
 import org.jruby.RubyIO;
+import org.jruby.ext.stringio.StringIO;
 import org.jruby.util.IOInputStream;
 import org.jruby.util.ByteList;
 import org.jruby.anno.JRubyMethod;
@@ -43,5 +44,120 @@ public class Varinter {
     RubyString bit_string = runtime.newString(new ByteList(backing.toByteArray(), true));
     bit_string.force_encoding(context, runtime.getEncodingService().getEncoding(org.jcodings.specific.ASCIIEncoding.INSTANCE));
     return bit_string;
+  }
+
+  @JRubyMethod
+  public static IRubyObject decode_varint( ThreadContext context, IRubyObject self, IRubyObject recv ) throws IOException {
+    if (!(recv instanceof RubyIO || recv instanceof StringIO )) {
+      return context.nil;
+    }
+
+    if (recv instanceof StringIO) {
+      StringIO src = (StringIO) recv;
+
+      long tmp;
+      if ((tmp = ((RubyFixnum)(src.getbyte(context))).getLongValue()) >= 0) {
+        return context.getRuntime().newFixnum(tmp);
+      }
+
+      long result = tmp & 0x7f;
+      if ((tmp = ((RubyFixnum)(src.getbyte(context))).getLongValue()) >= 0) {
+        result |= tmp << 7;
+      } else {
+        result |= (tmp & 0x7f) << 7;
+        if ((tmp = ((RubyFixnum)(src.getbyte(context))).getLongValue()) >= 0) {
+          result |= tmp << 14;
+        } else {
+          result |= (tmp & 0x7f) << 14;
+          if ((tmp = ((RubyFixnum)(src.getbyte(context))).getLongValue()) >= 0) {
+            result |= tmp << 21;
+          } else {
+            result |= (tmp & 0x7f) << 21;
+            if ((tmp = ((RubyFixnum)(src.getbyte(context))).getLongValue()) >= 0) {
+              result |= tmp << 28;
+            } else {
+              result |= (tmp & 0x7f) << 28;
+              if ((tmp = ((RubyFixnum)(src.getbyte(context))).getLongValue()) >= 0) {
+                result |= tmp << 35;
+              } else {
+                result |= (tmp & 0x7f) << 35;
+                if ((tmp = ((RubyFixnum)(src.getbyte(context))).getLongValue()) >= 0) {
+                  result |= tmp << 42;
+                } else {
+                  result |= (tmp & 0x7f) << 42;
+                  if ((tmp = ((RubyFixnum)(src.getbyte(context))).getLongValue()) >= 0) {
+                    result |= tmp << 49;
+                  } else {
+                    result |= (tmp & 0x7f) << 49;
+                    if ((tmp = ((RubyFixnum)(src.getbyte(context))).getLongValue()) >= 0) {
+                      result |= tmp << 56;
+                    } else {
+                      return context.nil;
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+
+      return context.getRuntime().newFixnum(result);
+    }
+
+    if (recv instanceof RubyIO) {
+      RubyIO src = (RubyIO) recv;
+
+      long tmp;
+      if ((tmp = ((RubyFixnum)(src.getbyte(context))).getLongValue()) >= 0) {
+        return context.getRuntime().newFixnum(tmp);
+      }
+
+      long result = tmp & 0x7f;
+      if ((tmp = ((RubyFixnum)(src.getbyte(context))).getLongValue()) >= 0) {
+        result |= tmp << 7;
+      } else {
+        result |= (tmp & 0x7f) << 7;
+        if ((tmp = ((RubyFixnum)(src.getbyte(context))).getLongValue()) >= 0) {
+          result |= tmp << 14;
+        } else {
+          result |= (tmp & 0x7f) << 14;
+          if ((tmp = ((RubyFixnum)(src.getbyte(context))).getLongValue()) >= 0) {
+            result |= tmp << 21;
+          } else {
+            result |= (tmp & 0x7f) << 21;
+            if ((tmp = ((RubyFixnum)(src.getbyte(context))).getLongValue()) >= 0) {
+              result |= tmp << 28;
+            } else {
+              result |= (tmp & 0x7f) << 28;
+              if ((tmp = ((RubyFixnum)(src.getbyte(context))).getLongValue()) >= 0) {
+                result |= tmp << 35;
+              } else {
+                result |= (tmp & 0x7f) << 35;
+                if ((tmp = ((RubyFixnum)(src.getbyte(context))).getLongValue()) >= 0) {
+                  result |= tmp << 42;
+                } else {
+                  result |= (tmp & 0x7f) << 42;
+                  if ((tmp = ((RubyFixnum)(src.getbyte(context))).getLongValue()) >= 0) {
+                    result |= tmp << 49;
+                  } else {
+                    result |= (tmp & 0x7f) << 49;
+                    if ((tmp = ((RubyFixnum)(src.getbyte(context))).getLongValue()) >= 0) {
+                      result |= tmp << 56;
+                    } else {
+                      return context.nil;
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+
+      return context.getRuntime().newFixnum(result);
+    }
+
+    return context.nil;
   }
 }
